d1758ed48798711849fa113876734bba9b8cff2b
Evgeny Svirsky <evgeny@cere.io>
2020-03-24 23:01:58 +0300

    Added ex 04 solution.

--- lesson-03/exercise-04.js
+++ lesson-03/exercise-04.js
@@ -1,5 +1,21 @@
 // Add new node to the provided Linked List to the specific index with value
 
 const addItem = (node, index, val) => {
+  let current = node;
+  let prev = node;
+  let currentIndex = 0;
 
+  while (prev.next !== null) {
+    if (currentIndex === index) {
+      prev.next = {
+        val,
+        next: current,
+      };
+    }
+
+    prev = current;
+    current = current.next;
+
+    currentIndex++;
+  }
 };
