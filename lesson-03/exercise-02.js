2057e118b54e6f46c4a578509d9fe099562642fb
Evgeny Svirsky <evgeny@cere.io>
2020-03-24 21:29:38 +0300

    Added solutions.

--- lesson-03/exercise-02.js
+++ lesson-03/exercise-02.js
@@ -1,5 +1,24 @@
 // Exercise 02: Return the most frequent item in the array
 
 const mostFrequent = (array) => {
-  // Solution here...
+  if (array.length === 0) {
+    return null;
+  }
+
+  let map = new Map();
+
+  array.forEach(item => {
+    map.set(item, map.has(item) ? map.get(item) + 1 : 1);
+  });
+
+  let max = 0;
+  let maxItem;
+  for (let item of map.entries()) {
+    if (item[1] > max) {
+      max = item[1];
+      maxItem = item[0];
+    }
+  }
+
+  return maxItem;
 };
