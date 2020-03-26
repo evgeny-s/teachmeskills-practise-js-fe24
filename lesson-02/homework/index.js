d7bf0a73120e99099add5eb49b683a4da868793a
AntonKurdo <anton.kurdo@gmail.com>
2020-03-26 13:34:55 +0300

    lesson 02 (Homework)

--- lesson-02/homework/index.js
+++ lesson-02/homework/index.js
@@ -6,3 +6,20 @@
 // Dogs should be refreshed each time user clicked on `Show dogs` button
 // Create simple UI with CSS
 // Forbidden to use any of JS or CSS framework
+
+let url = 'https://dog.ceo/api/breeds/image/random';
+let ul = document.querySelector('ul');
+
+document.querySelector('.btn_js').addEventListener('click', () => {
+    for (let i = 0; i <= 9; i++) {
+        ul.innerHTML = '';
+        fetch(url).then(res => {
+            return res.json();
+        }).then(res => {
+            let li = document.createElement('li');
+            li.innerHTML = `<img src='${res.message}'>`;
+            ul.appendChild(li);
+        })
+    }
+})
+
