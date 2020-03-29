8e911cce09a098cbc79771c1377f0bac9aa8d916
Evgeny Svirsky <evgeny@cere.io>
2020-03-29 15:28:09 +0300

    Added lesson-05 task.

---
+++ lesson-05/homework/index.js
@@ -0,0 +1,5 @@
+// Homework
+// Create simple registration form which includes email, password, re-type password, firstName, lastName. Once user registered show him his profile with his data. Store data inside localStorage.
+// Create logout button inside profile
+// Create login form in order to sign in by registered email.
+// Use modules and webpack in order to build this project
