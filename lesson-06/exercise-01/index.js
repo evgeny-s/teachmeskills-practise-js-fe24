af6b0eabe9858bc6201ff9cc90fdfba73dbf40a5
Evgeny Svirsky <evgeny@cere.io>
2020-04-01 23:48:57 +0300

    Added tasks for the lesson-06.

---
+++ lesson-06/exercise-01/index.js
@@ -0,0 +1,5 @@
+// Clone repo: git clone http://github.com/Slashgear/webpack-workshop.git
+// Change dir to the folder: `cd webpack-workshop`
+// Run npm i
+// Open project in your browser: http://localhost:8080
+// Start workshop (Novice section)
