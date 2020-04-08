3ac33630530b537b96d88932450a1c8a86d97daf
Evgeny Svirsky <evgeny@cere.io>
2020-04-08 23:35:37 +0300

    * Added tasks for lesson-08

---
+++ lesson-08/exercise-02.js
@@ -0,0 +1,5 @@
+// Exercise 02
+// Create brand new ReactJS application with `create-react-app` command.
+// Create simple clock application. A basic digital clock that displays the current date and time, and a toggle switch to toggle date on and off.
+// https://user-images.githubusercontent.com/33935506/33753477-2fa5b182-dbf0-11e7-8608-c69a8739b09e.PNG
+// Try to use as much separated components as possible
---
+++ lesson-08/exercise-03.js
@@ -0,0 +1,7 @@
+// Exercise 03
+// Create brand new ReactJS application with `create-react-app` command.
+// Create timer application. A countdown timer that offers an advanced UI experience. The Timer uses a Keypad to enter hours, minutes, and seconds into a Display. The countdown timer changes to fullscreen when the Timer is started. A control panel allows one to start, stop, resume, and reset the timer.
+// https://user-images.githubusercontent.com/33935506/33770696-521ef0ec-dc37-11e7-8d48-8c1d55929c22.PNG
+// https://user-images.githubusercontent.com/33935506/33770697-5254cb86-dc37-11e7-8c36-de79804b7bfc.PNG
+// https://user-images.githubusercontent.com/33935506/33770698-52879020-dc37-11e7-8c4b-634a95a34079.PNG
+// Try to use as much separated components as possible
