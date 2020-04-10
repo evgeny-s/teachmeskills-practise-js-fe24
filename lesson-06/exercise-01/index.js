1947f704d2ab4616c6a17379efa4c9b180eacd6e
Anton Kurdo <56073388+AntonKurdo@users.noreply.github.com>
2020-04-10 18:03:15 +0300

    a (#3)
    * Added lesson-04 task.
    
    * Added lesson-05 task.
    
    * Added lesson-05 task.
    
    * Added lesson-05 task.
    
    * Added lesson-05 task.
    
    * Fix for task lesson-05.
    
    * Fix for task lesson-05.
    
    * Added ui picture.
    
    * Added tasks for the lesson-06.
    
    * Added start command.
    
    * Added demo for History API.
    
    * Added demo for one way and two way binding.
    
    * Added demo for reactive programming.
    
    * * Added exercise for the lesson-07.
    
    * * Added tasks for lesson-08
    
    Co-authored-by: Evgeny Svirsky <evgeny@cere.io>

---
+++ lesson-06/exercise-01/index.js
@@ -0,0 +1,6 @@
+// Clone repo: git clone http://github.com/Slashgear/webpack-workshop.git
+// Change dir to the folder: `cd webpack-workshop`
+// Run npm i
+// Start the project: `npm run docs:dev`
+// Open project in your browser: http://localhost:8080
+// Start workshop (Novice section)
---
+++ lesson-08/exercise-01.js
@@ -0,0 +1,5 @@
+// Exercise 01
+// Create brand new ReactJS application with `create-react-app` command.
+// Create simple clicker application. A basic React app that allows one to increase, decrease, or reset a counter.
+// https://user-images.githubusercontent.com/33935506/33752825-a81c5ed0-dbec-11e7-8041-44d1a5b2ca46.PNG
+// Try to use as much separated components as possible
