ebabe9a008d681f8e64e51878a936489be6a5dd7
Anton Kurdo <56073388+AntonKurdo@users.noreply.github.com>
2020-04-14 21:54:56 +0300

    ы (#4)
    * Added lesson-04 task.
    
    * Added lesson-05 task.
    
    * Added lesson-05 task.
    
    * Added lesson-05 task.
    
    * Added lesson-05 task.
    
    * Fix for task lesson-05.
    
    * Fix for task lesson-05.
    
    * Added ui picture.
    
    * Added tasks for the lesson-06.
    
    * Added start command.
    
    * Added demo for History API.
    
    * Added demo for one way and two way binding.
    
    * Added demo for reactive programming.
    
    * * Added exercise for the lesson-07.
    
    * * Added tasks for lesson-08
    
    * * Added task for the lesson-09.
    
    * * Added tasks for lesson-09.
    
    Co-authored-by: Evgeny Svirsky <evgeny@cere.io>

---
+++ lesson-09/exercise-02/index.js
@@ -0,0 +1,19 @@
+// Exercise 02
+// Create brand new ReactJS application with `create-react-app` command.
+// Create ReactJS application. Initial page should be Cart Checkout form with fields:
+// - First Name - text input
+// - Last Name - text input
+// - Delivery (yes or no) - radio
+// If Delivery === yes, specify address:
+// - Country (use `countries.json`) - dropdown
+// - State (use `states.json`) - dropdown - should depend on country
+// - Cities (use `cities.json`) - dropdown - should depend on state
+// - Add button with fetching geolocation of the browser API
+
+// - Payment type (cash or card)
+// If Payment type === card, show:
+// - Card Number - input
+// - Exp Date - input
+// - CVV - input
+
+// After user submit the form, data should be saved to localStorage.
---
+++ lesson-09/exercise-03.js
@@ -0,0 +1,5 @@
+// Exercise 03
+// Create brand new ReactJS application with `create-react-app` command.
+// Create ReactJS application. A weather application that displays the current weather, daily forecasts, and hourly forecasts based on your current geolocation. The application integrates with both the OpenWeather API and Google Geolocation API.
+// https://user-images.githubusercontent.com/33935506/34460233-590c5d68-ee11-11e7-9b77-f1c3d8d7536b.png
+// Try to use as much separated components as possible
