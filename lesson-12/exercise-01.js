0a16941cdd782305f3bc82784667ba2844e20bc9
Evgeny Svirsky <evgeny@cere.io>
2020-04-24 18:28:26 +0300

    * Added lesson-12 tasks.

---
+++ lesson-12/exercise-01.js
@@ -0,0 +1,17 @@
+/*
+  Create brand new ReactJS application with `create-react-app` command.
+  Application should use ONLY React Hooks. No class components!
+  Create COVID-19 monitor application
+  It should consist of 2 separate pages (add React Router):
+    * Add Data Form
+    * Show Chart for the specific city
+  Form should consist of the following:
+    * Date (connect this lib: https://www.npmjs.com/package/react-bootstrap-date-picker)
+    * City (text field)
+    * Infected amount (text field)
+  Chart page should consist of:
+    * Dropdown with the list of the cities.
+    * Chart (use this lib: https://canvasjs.com/react-charts/json-data-api-ajax-chart/)
+
+  Data should be persisted into the Local Storage.
+ */
---
+++ lesson-12/exercise-02.js
@@ -0,0 +1,6 @@
+/*
+  Create brand new ReactJS application with `create-react-app` command.
+  Create tic-tac-toe game:
+  https://drive.google.com/open?id=1YqRm_dbs194qtjPvTkwe5Rjn2gzFaECV
+  https://drive.google.com/open?id=13oznubF4vPYYQf8KT_fcBgIhM36MzC0a
+ */
