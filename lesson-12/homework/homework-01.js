e0e625be4d5209e866a5cd1c7d8051598ddc0af4
Evgeny Svirsky <evgeny@cere.io>
2020-04-24 23:42:06 +0300

    Added more exercises.

---
+++ lesson-12/homework/homework-01.js
@@ -0,0 +1,7 @@
+/*
+  Create brand new ReactJS application with `create-react-app` command.
+  Application should use ONLY React Hooks. No class components!
+  Create TODO application: https://drive.google.com/open?id=1mOvuAr_rN8y0MO0RD4ItQrq13CCoSyve
+
+  Finish app, upload to GitHub and send to the teacher.
+ */
---
+++ lesson-12/homework/homework-02.js
@@ -0,0 +1,7 @@
+/*
+  Create brand new ReactJS application with `create-react-app` command.
+  Application should use ONLY React Hooks. No class components!
+  Create Calculator application: https://drive.google.com/open?id=1jVR8uAWfAPTQ617_TPRceb_2bu2DUvON
+
+  Finish app, upload to GitHub and send to the teacher.
+ */
