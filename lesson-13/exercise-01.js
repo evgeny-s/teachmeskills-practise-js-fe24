45ced4f32b7036277c0148293d5080875517c20b
Evgeny Svirsky <evgeny@cere.io>
2020-05-04 22:51:33 +0300

    * Added lesson 13 tasks.

---
+++ lesson-13/exercise-01.js
@@ -0,0 +1,4 @@
+/*
+  Create brand new ReactJS application with `create-react-app` command.
+  Create TODO application using Redux
+ */
---
+++ lesson-13/exercise-02.js
@@ -0,0 +1,9 @@
+/*
+  Create brand new ReactJS application with `create-react-app` command.
+  Create Light Pad application using Redux:
+  https://drive.google.com/open?id=1gvvntzAA4kRjh4-b0Z1QgL8oYbwL6cim
+
+  Assets:
+  http://res.cloudinary.com/dtjoxh5wb/image/upload/v1495532392/on_med_n3p8pd.png
+  http://res.cloudinary.com/dtjoxh5wb/image/upload/v1495532392/off_med_fmma6p.png
+ */
