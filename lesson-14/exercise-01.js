aacd8265e882686efd1aecd0f609af4312414ab7
Evgeny Svirsky <evgeny@cere.io>
2020-05-08 16:40:11 +0300

    Added lesson-14 exercise.

---
+++ lesson-14/exercise-01.js
@@ -0,0 +1,17 @@
+/*
+  Create brand new ReactJS application with `create-react-app` command.
+  Create Webshop application using Redux
+
+  It should consist of 2 reducers: user and shop.
+    - `user` reducer should be responsible for authentication
+    - `shop` reducer should be responsible for webshop implementation
+
+  Authentication:
+    - It should be simple form with login/password. (Hardcode existing users for now and check auth on Frontend)
+  Shop:
+    - It should be 2 pages:
+      - Webshop Products - list with a possibility to send products to the cart (get from data.json)
+      - My Cart:
+          * list of items in the card. Item can be cancelled.
+          * for for checkout. After use checked out - redirect to the products list
+ */
