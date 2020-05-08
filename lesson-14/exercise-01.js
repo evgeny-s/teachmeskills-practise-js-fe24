32fe0b27905d2bd2115113296993299f4de94256
Evgeny Svirsky <evgeny@cere.io>
2020-05-08 19:33:00 +0300

    * Added requirements.

--- lesson-14/exercise-01.js
+++ lesson-14/exercise-01.js
@@ -14,4 +14,29 @@
       - My Cart:
           * list of items in the card. Item can be cancelled.
           * for for checkout. After use checked out - redirect to the products list
+
+1. Implement Login component
+  - It should have 2 inputs: username, password
+  - It should be 2 hardcoded users in the reducers: id, username, password
+  - Add authenticated to the user reducer
+
+2. Add Router for components:
+  - Login: /login
+  - Products: /products
+  - MyCart: /cart
+
+3. All components should be available only for authenticated users (add validation)
+
+4. Implement Products component:
+  - It should pull products from data.json
+  - Each item should have `To Cart` button
+  - Once user clicked on button - save item to store.
+
+5. Implement MyCart component:
+  - It should have a list of selected products
+  - It should be form:
+    * Full Name
+    * Address
+  - It should have "Submit" button.
+  - After user submitted form - save data to store.
  */
