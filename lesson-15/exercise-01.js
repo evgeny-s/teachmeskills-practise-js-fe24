184b6dc529f9a58bff808fb9f6e309ecd7d3a867
Evgeny Svirsky <evgeny@cere.io>
2020-05-11 23:19:50 +0300

    * Added lesson-15 exercise.

---
+++ lesson-15/exercise-01.js
@@ -0,0 +1,19 @@
+/*
+  Create brand new ReactJS application with `create-react-app` command.
+  Create Medium like application using Redux+Saga
+
+  It should consist of the following pages:
+
+  1. Articles List page (show all articles with image, title and description)
+      - should be button for adding new article
+      - should be a possibility to remove article
+      - should be a possibility to edit article
+  2. Create Article page (should be form with fields:
+      - name,
+      - image,
+      - short_description,
+      - text,
+  3. Each article should have comments section on the bottom
+      - comment should have text.
+      - one user can add only 10 comments per article.
+ */
