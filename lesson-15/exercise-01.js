5305a5776b99253e70de3cd17e4009e5de179d06
Evgeny Svirsky <evgeny@cere.io>
2020-05-12 21:30:51 +0300

    * Added spec for lesson-15/exercise-01

--- lesson-15/exercise-01.js
+++ lesson-15/exercise-01.js
@@ -9,11 +9,47 @@
       - should be a possibility to remove article
       - should be a possibility to edit article
   2. Create Article page (should be form with fields:
-      - name,
+      - title,
       - image,
-      - short_description,
+      - description,
       - text,
   3. Each article should have comments section on the bottom
       - comment should have text.
       - one user can add only 10 comments per article.
+
+  User json-server for API.
+
+
+  Steps to implement:
+    1. List:
+      - Create ArticleList component
+      - Create ArticleList container
+      - Connect container to reducer
+    2. Connect List to sagas
+    3. Create Routing for the application:
+      - /articles -> ArticleList
+      - /create-article -> ArticleEdit
+    4. Add create article form
+      - Add bootstrap form with fields
+        * title
+        * image
+        * description
+        * text
+      - Add dispatch for form submission
+      - Send data to the server
+      - Redirect to list after new article added
+    5. Add Edit/Delete buttons to the Articles list
+    6. Implement Delete button
+      - Add saga function
+      - Add service method for removing article by id
+    6. Implement Edit button
+      - Add saga function
+      - Add service method for editing article by id (the same ArticleEdit component should be used)
+    7. Add handler for ARTICLE/FETCH_ERROR action.
+    8. Add Comments component to the article
+      - It should return list of comments for the specific article.
+    9. Add CommentsForm component to be able to add new component
+      - Fields should be used:
+        * text
+        * articleId
  */
